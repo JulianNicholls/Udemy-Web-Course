343f09dc97bd4cd53b1f2203b67f27626d6ee3b9
Julian Nicholls <juliannicholls29@gmail.com>
2015-07-20 11:16:41 +0100

    Code Player is working.

--- codeplayer/codeplayer.js
+++ codeplayer/codeplayer.js
@@ -3,34 +3,40 @@ $(function() {
 
     $('.toggle').click(function() {
         var $this   = $(this),
-            column  = '#' + $this.text().toLowerCase(),
-            $column = $(column);
+            $column = $('#' + $this.text().toLowerCase());
 
         $this.toggleClass('enabled');
         $column.toggleClass('enabled');
 
         resizeColumns();
     });
+
+    $('#run').click( function() {
+        $('iframe').contents().find("html").html(
+            '<style>' + $('#cssText').val() + '</style>' + $('#htmlText').val()
+        );
+
+        document.getElementById('result-frame').contentWindow.eval($('#jsText').val());
+    });
 });
 
 function resizeColumns() {
     var count = visibleColumns(),
-        width = Math.floor(100 / count) - 1;
+        width = 100 / count;        // box-sizing: border-box takes care of padding / border etc
 
     $('.column').each(function() {
         $(this).css('width', width + '%')
     })
 
+    var winHeight   = $(window).height(),
+        headHeight  = $('header').outerHeight(),
+        colHeight   = winHeight - headHeight - 10;
+
+    $('.column').height(colHeight + 'px')
 }
 
 function visibleColumns() {
-    var count = 0;
-
-    $('.column').each( function() {
-        if($(this).hasClass('enabled')) {
-            ++count;
-        }
-    });
-
-    return count;
+    return $('.column').filter( function() {
+        return $(this).hasClass('enabled');
+    }).length;
 }
